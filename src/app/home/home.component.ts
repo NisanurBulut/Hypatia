95d1554b4c1fb50e96feee3d4f7091d241e5d8e8
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-13 14:27:34 +0300

    video ve resimler için kaynakalr olusturuldu ve slide gosterımınde link tanımlamaları yapıldı

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  homeImageList = [
+    {
+      imagePath: '../../assets/images/dogru.PNG',
+      title: 'Doğru Kesişim Gösterimi',
+      description: 'Aynı düzlemdeki 3 çemberi teğet geçen doğruların kesişim noktaları daima doğrusaldır.',
+      videoPath: '../../assets/videos/dogru.webm',
+      videoLink: '/videos/item1'
+    },
+    {
+      imagePath: '../../assets/images/dogru.PNG',
+      title: 'Doğru Kesişim Gösterimi',
+      description: 'Aynı düzlemdeki 3 çemberi teğet geçen doğruların kesişim noktaları daima doğrusaldır.',
+      videoPath: '../../assets/videos/dogru.webm',
+      videoLink: '/videos/item1'
+    },
+    {
+      imagePath: '../../assets/images/dogru.PNG',
+      title: 'Doğru Kesişim Gösterimi',
+      description: 'Aynı düzlemdeki 3 çemberi teğet geçen doğruların kesişim noktaları daima doğrusaldır.',
+      videoPath: '../../assets/videos/dogru.webm',
+      videoLink: '/videos/item1'
+    },
+    {
+      imagePath: '../../assets/images/dogru.PNG',
+      title: 'Doğru Kesişim Gösterimi',
+      description: 'Aynı düzlemdeki 3 çemberi teğet geçen doğruların kesişim noktaları daima doğrusaldır.',
+      videoPath: '../../assets/videos/dogru.webm',
+      videoLink: '/videos/item1'
+    }
+  ];
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  preventNormal(event: MouseEvent, image: any) {
+    if (!image.prevented) {
+      event.preventDefault();
+      this.router.navigate(['./videos']);
+    }
+  }
 }
