462a8539b30d6b4f5a3025c467265df9e5fc739e
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-13 22:04:00 +0300

    json formatta veri seti eklendi.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient } from '@angular/common/http';
 
 // third party imports
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -26,6 +27,7 @@ import { SafePipe } from './Utility/safe.pipe';
     CarouselModule.forRoot(),
     BsDropdownModule.forRoot(),
     BrowserAnimationsModule,
+    HttpClient,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,32 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css']
 })
-export class VideoListComponent implements OnInit {
+export class VideoListComponent implements OnInit, OnDestroy {
   title = 'Hypatia!';
+  private req: any;
   videoListesi = [
     {
       name: 'Video1',
       slug: 'item1',
-      description:'Aynı düzlemdeki 3 çemberi teğet geçen doğruların kesişim noktaları daima doğrusaldır.',
-      embed:'../../assets/Videos/dogruKesisim.webm'
+      description: 'Aynı düzlemdeki 3 çemberi teğet geçen doğruların kesişim noktaları daima doğrusaldır.',
+      embed: '../../assets/Videos/dogruKesisim.webm'
     },
     {
       name: 'video2',
       slug: 'item2',
-      description:'Dairenin alanı, üçgenin alanı üzerinden görsel olarak anlatılmış.',
-      embed:'../../assets/Videos/daireAlan.webm'
+      description: 'Dairenin alanı, üçgenin alanı üzerinden görsel olarak anlatılmış.',
+      embed: '../../assets/Videos/daireAlan.webm'
     }
   ];
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.req = this.http.get('assets/dataset/videos.json')
+      .subscribe(data => {
+        console.log(data);
+      });
   }
-getEmbedUrl(item){
+  ngOnDestroy() {
+    this.req.unsubscribe();
+  }
+  getEmbedUrl() {
 
-}
+  }
 }
